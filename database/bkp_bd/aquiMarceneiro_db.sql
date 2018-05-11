73b99724f7a5eafe7902abd1e7cbc142cf0f7cb2
danilimas22 <danilimas22@hotmail.com>
2018-05-11 14:42:38 -0300

    correção do banco

--- database/bkp_bd/aquiMarceneiro_db.sql
+++ database/bkp_bd/aquiMarceneiro_db.sql
@@ -11,8 +11,54 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Copiando estrutura do banco de dados para aqui_marceneiro
+CREATE DATABASE IF NOT EXISTS `aqui_marceneiro` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `aqui_marceneiro`;
+
+-- Copiando estrutura para tabela aqui_marceneiro.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela aqui_marceneiro.password_resets
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  KEY `password_resets_email_index` (`email`),
+  KEY `password_resets_token_index` (`token`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 -- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela aqui_marceneiro.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `cpf_cnpj` varchar(14) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `uf` char(2) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cidade` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cep` varchar(8) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `descricao` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `especialidade` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tipo` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `foto_usuario` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT 'default.jpg',
+  `rua` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bairro` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL,
+  `latitude` decimal(10,8) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Exportação de dados foi desmarcado.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
